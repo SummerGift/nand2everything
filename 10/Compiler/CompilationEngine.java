5b32b9a188aa370d8167f31a92995fa1635d6372
SummerGift <summergift2019@gmail.com>
2020-12-29 10:08:26 +0800

    [update] compilationEngine

--- 10/Compiler/CompilationEngine.java
+++ 10/Compiler/CompilationEngine.java
@@ -1,11 +1,56 @@
+
 /**
  * CompilationEngine
  */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CompilationEngine {
 
     public static void main(String[] args) {
-        System.out.println("hello summer");
+
+        String sourFile = "D:\\work\\nand2everything\\10\\Test\\ArrayTest\\Main.jack";
+        String desFile = "D:\\work\\nand2everything\\10\\Test\\ArrayTest\\Main.jack_temp";
+
+        try {
+            String stringLine = null;
+            String wtrteLine = null;
+
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(sourFile));
+            BufferedReader read = new BufferedReader(isr);
+            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(desFile));
+            BufferedWriter write = new BufferedWriter(osw);
+            
+            StringBuilder fBuilder = new StringBuilder();
+
+            while ((stringLine = read.readLine()) != null) {
+
+                if (stringLine.equals("")) {
+                    continue;
+                }
+
+                if (stringLine.startsWith("//")){
+                    continue;
+                }
+
+                Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+                Matcher m = p.matcher(stringLine);
+                wtrteLine = m.replaceAll("");
+                
+                fBuilder.append(wtrteLine);
+            }
+
+            write.write(fBuilder.toString());
+            write.close();
+            read.close();
+
+            System.out.println(fBuilder.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    
 }
