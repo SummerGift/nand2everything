c6d630d66e64e38e3c6c2608e6a81c60acb27d17
SummerGift <summergift2019@gmail.com>
2021-02-14 21:47:19 +0800

    [add] antlr_jar environment for windows

---
+++ 10/antlr_jar/antlr.bat
@@ -0,0 +1,2 @@
+java -cp D:\work\antlr_jar\antlr-4.9-complete.jar;%CLASSPATH% org.antlr.v4.Tool %*
+
---
+++ 10/antlr_jar/grun.bat
@@ -0,0 +1,2 @@
+java -cp ".;D:\work\antlr_jar\antlr-4.9-complete.jar" org.antlr.v4.gui.TestRig %*
+
