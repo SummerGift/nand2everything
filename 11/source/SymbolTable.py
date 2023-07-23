9d29426b34f57fdc6e1541fbd876f2cebddb2bdd
SummerGift <summergift2019@gmail.com>
2023-07-23 17:58:31 +0800

    [update] change the implentation of symboltable

--- 11/source/SymbolTable.py
+++ 11/source/SymbolTable.py
@@ -1,52 +1,44 @@
 
-from JackDefine import *
-
-# we will cteate a list of symbol tables, one per scope
 class SymbolTable():
+    """
+    Creates the current symbpls for a given class or subrotine
+    """
     def __init__(self):
-        self.class_symbols = {}
-        self.subroutine_symbols = {}
-        self.symbols = {SK_STATIC:self.class_symbols, SK_FIELD:self.class_symbols, 
-                SK_ARG:self.subroutine_symbols, SK_VAR:self.subroutine_symbols}
-        self.index = {SK_STATIC:0, SK_FIELD:0, SK_ARG:0, SK_VAR:0}
+        self.symbols = []
 
-    def __str__(self):
-        return self.symbol_string('class', self.class_symbols)    \
-             + self.symbol_string('subroutine', self.subroutine_symbols)
+    def reset(self):
+        """starts a new subroutine scope,"""
+        self.symbols = []
 
-    def symbol_string(self, name, table):
-        result = 'symbol table '+name+':\n'
-        for n, (t, k, i) in table.items():
-            result += 'symbol name:'+n+', type:'+t+', kind:'+k+', index:'+str(i)+'\n'
-        return result
+    def define(self, name, symbol_type, kind):
+        """defines a new identifier of the given name, type, and kind and assigns it a running index"""
 
-    def start_subroutine(self):
-        self.subroutine_symbols.clear()
-        self.index[SK_ARG] = self.index[SK_VAR] = 0
-
-    def define(self, name, type, kind):
-        self.symbols[kind][name] = (type, kind, self.index[kind])
-        self.index[kind] += 1
+        new_symbol = {
+            "name": name,
+            "type": symbol_type,
+            "kind": kind,
+            "index": self.var_count(kind)
+        }
+        self.symbols.append(new_symbol)
 
     def var_count(self, kind):
-        return sum(1 for n, (t, k, i) in self.symbols[kind].items() if k == kind)
+        """return the number of variables of the given kind already defined in the current scope"""
+        return sum(symbol['kind'] == kind for symbol in self.symbols)
+    
+    def kind_of(self, name):
+        """return the kind of the name identifer in the current scope(STATIC, FIDLE, ARG, VAR, NONE)"""
+        return self.find_symbol_by_name(name).get("kind")
     
     def type_of(self, name):
-        (type, kind, index) = self.lookup(name)
-        return type
-
-    def kind_of(self, name):
-        (type, kind, index) = self.lookup(name)
-        return kind
-
+        """return the type of the named identifier in  the current scope"""
+        return self.find_symbol_by_name(name).get("type")
+    
     def index_of(self, name):
-        (type, kind, index) = self.lookup(name)
-        return index
-
-    def lookup(self, name):
-        if name in self.subroutine_symbols:
-            return self.subroutine_symbols[name]
-        elif name in self.class_symbols:
-            return self.class_symbols[name]
-        else:
-            return (None, None, None)
+        """return the type of the named identifier in  the current scope"""
+        return self.find_symbol_by_name(name).get("index")
+    
+    def find_symbol_by_name(self, name):
+        """traverse all symbols to fine identifer name"""
+        for symbol in self.symbols:
+            if symbol["name"] == name:
+                return symbol
